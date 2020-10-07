ea5055d8d0460e3ce5119b91db568fa48e01b0fb
TRI AFRILIYANTI <71365606+triafri@users.noreply.github.com>
2020-10-07 10:19:44 +0700

    Add files via upload

---
+++ tugas2.php
@@ -0,0 +1,48 @@
+<?php
+$menu1="Cireng";
+$menu2="Es Duren";
+$menu3="Es Milo";
+$menu4="Es Teh";
+$menu5="Piscok";
+$harga1=1000;
+$harga2=15000;
+$harga3=20000;
+$harga4=5000;
+$harga5=1000;
+
+echo "<br>";
+echo "<h1>DAFTAR MENU</h1>";
+echo "<br>";
+echo "<table width = 500 border =1 >
+		<tr align=center>
+			<td>No</td>
+			<td>Nama Menu</td>
+			<td>Harga</td>
+		</tr>
+		<tr align=center>
+			<td>1</td>
+			<td>".$menu1."</td>
+			<td>".$harga1."</td>
+		</tr>
+		<tr align=center>
+			<td>2</td>
+			<td>".$menu2."</td>
+			<td>".$harga2."</td>
+		</tr>
+		<tr align=center>
+			<td>3</td>
+			<td>".$menu3."</td>
+			<td>".$harga3."</td>
+		</tr>
+		<tr align=center>
+			<td>4</td>
+			<td>".$menu4."</td>
+			<td>".$harga4."</td>
+		</tr>
+		<tr align=center>
+			<td>5</td>
+			<td>".$menu5."</td>
+			<td>".$harga5."</td>
+		</tr>
+		</table>";
+?>
